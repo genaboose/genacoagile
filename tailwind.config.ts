a23963ac51c2ba655a2c438d89c8d0f28b484c3a
genaboose <georgnauerz@gmail.com>
2025-09-18 12:07:59 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,16 @@
 import type { Config } from 'tailwindcss'
 const config: Config = {
   content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          navy: '#0c255d',
+          gold: '#ffcd59'
+        }
+      }
+    }
+  },
   plugins: [],
 }
 export default config
