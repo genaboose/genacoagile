eab09ce58efc1e7e025972e0a4ba3cf53e62d09f
genaboose <georgnauerz@gmail.com>
2025-10-09 22:42:54 +0200

    Update page.tsx

--- app/impressum/page.tsx
+++ app/impressum/page.tsx
@@ -4,12 +4,14 @@ export default function Impressum() {
       <h1 className="text-3xl font-bold">Impressum</h1>
       <p className="mt-4 text-slate-700">GENACO Agile</p>
       <p className="mt-4 text-slate-700">Ein Brand der GENACO UG ( Haftungsbeschränkt)</p>
-      <p className="mt-4 text-slate-700">In Unterstützung der GNC Georg Nauerz Consulting GmbH</p>
-      <p className="mt-4 text-slate-700">GENACO Agile</p>
-      <p className="mt-4 text-slate-700">GENACO Agile</p>
-      <p className="mt-4 text-slate-700">GENACO Agile</p>
-      <p className="mt-4 text-slate-700">GENACO Agile</p>
-      <p className="mt-4 text-slate-700">GENACO Agile</p>
-    </main>
+      <p className="mt-4 text-slate-700">In Unterstützung durch die GNC Georg Nauerz Consulting GmbH</p>
+      <p className="mt-4 text-slate-700">Adresse:</p>
+      <p className="mt-4 text-slate-700">Kinderlehre 14, 67677 Enkenbach-Alsenborn</p>
+      <p className="mt-4 text-slate-700">Telefon: +49 6303 5045</p>
+      <p className="mt-4 text-slate-700">Geschäftsführer der GENACO UG: Emma Nauerz</p>
+      <p className="mt-4 text-slate-700">Anschrift wie oben</p>
+      <p className="mt-4 text-slate-700">Sitz der Gesellschaft: Enkenbach-Alsenborn</p>
+      <p className="mt-4 text-slate-700">Registergericht und Nummer: AG Kaiserslautern, HRB 33555</p>
+     </main>
   );
 }
