b3b9c9c5a739f55e2a94a723637cfc6011d1eb67
genaboose <georgnauerz@gmail.com>
2025-11-11 14:03:17 +0100

    Update page.tsx

--- app/impressum/page.tsx
+++ app/impressum/page.tsx
@@ -6,11 +6,10 @@ export default function Impressum() {
       <p className="mt-4 text-slate-700">Ein Brand der GENACO UG ( Haftungsbeschränkt)</p>
       <p className="mt-4 text-slate-700">In Unterstützung durch die GNC Georg Nauerz Consulting GmbH</p>
       <p className="mt-4 text-slate-700">Adresse:</p>
-      <p className="mt-4 text-slate-700">Kinderlehre 14, 67677 Enkenbach-Alsenborn</p>
+      <p className="mt-4 text-slate-700">Eisenbahnstraße 42, 67655 Kaiserslautern</p>
       <p className="mt-4 text-slate-700">Telefon: +49 6303 5045</p>
       <p className="mt-4 text-slate-700">Geschäftsführer der GENACO UG: Emma Nauerz</p>
       <p className="mt-4 text-slate-700">Anschrift wie oben</p>
-      <p className="mt-4 text-slate-700">Sitz der Gesellschaft: Enkenbach-Alsenborn</p>
       <p className="mt-4 text-slate-700">Registergericht und Nummer: AG Kaiserslautern, HRB 33555</p>
       <p className="mt-4 text-slate-700">Inhaltlich Verantwortlicher gemäß §55 Abs. 2 RStV: Georg Nauerz (Anschrift wie oben)</p>
       <p className="mt-4 text-slate-700">Haftungshinweis: Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich. Weitere Informationen erhalten Sie nachstehend.</p>
